11963d30e46f3142088ddd9a0ae6ea967e2b1b56
Poonam0107 <120553133+Poonam0107@users.noreply.github.com>
2023-02-09 01:25:37 +0530

    players

---
+++ Players.java
@@ -0,0 +1,25 @@
+package com.day11_12Assignment;
+
+import java.util.Scanner;
+
+public class Players {
+
+	public static void main(String[] args) {
+		        
+		/**
+		 * calling methods here in main method to run the program
+		 */
+		System.out.println("\nPress 1 > for view 4 players cards");
+		DeckOfCards play = new DeckOfCards();
+		Scanner scanner = new Scanner(System.in);
+		int press = scanner.nextInt();
+
+		switch (press) {
+		case 1:
+			play.getCardCombination();
+			play.getPlayer();
+			break;
+		}
+	}
+
+}
